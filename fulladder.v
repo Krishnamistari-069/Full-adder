1d2bc23d47a465f212eb87aa5fa2a942a98ab63d
Krishnamistari-069 <220170111069@vgecg.ac.in>
2025-08-20 22:10:16 +0530

    Update fulladder.v
    new updated code

--- fulladder.v
+++ fulladder.v
@@ -1,10 +1,10 @@
-module FA_1_Bit_BH(A,B,Cin,Sum,Cout);
+module FA_1_Bit_BH (
+    input A, B, Cin,
+    output reg Sum, Cout
+);
 
-input A,B,Cin;
- output reg Sum,Cout;
+always @(*) begin
+    {Cout, Sum} = A + B + Cin;
+end
 
- always @( A,B,Cin)
- begin
-{Cout,Sum} = A + B + Cin;
- end
- endmodule
+endmodule
